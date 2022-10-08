1153f9dae768c09ce8d767b36b3300799ea0b0f6
zaky <ahmed6mohamed6@gmail.com>
2022-10-08 10:46:59 +0200

    implementing login form page

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
-    <h1>Login comming soon ...</h1>
+    <h1>Login</h1>
+    
+    <br>
+    
+    <form @submit.prevent="submitLogin">
+      Email
+      <br/>
+      <input v-model="email" type="email"/>
+      <br/><br/>
+      Password
+      <br/>
+      <input v-model="password" type="password"/>
+      <br/><br/>
+      <button type="submit">Log In</button>
+    </form>
   </div>
 </template>
 
 <script>
 
+import axios from "axios";
+
 export default {
   name: 'LoginComponent',
   data() {
-    return {};
+    return {
+      email: '',
+      password: ''
+    };
   },
   methods: {
     login() {
+      // Prepare Data
+      const path = "login";
+      const url = process.env.VUE_APP_SERVER_HTTP + path;
+      const device_name = navigator.userAgent;
+
+      // Submit Login Request
+      axios
+          .post(url, {
+            email: this.email,
+            password: this.password,
+            device_name,
+          })
+          .then((response) => {
+            console.log("Token: " + response.data);
+            this.$router.push("/");
+          })
+          .catch((error) => console.log(error));
+
     }
   },
   apollo : {}
